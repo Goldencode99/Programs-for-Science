595d958fc75bf8ecf7c6e4b11693eadf097df3d8
Goldencode99 <auves99@icloud.com>
2014-04-25 15:24:46 -0700

    I just changed the terms

--- Heat_of_Solution.py
+++ Heat_of_Solution.py
@@ -1,4 +1,4 @@
 def calcEnthalpy(molarMass,gramsCompound,gramsWater,initTemp,finalTemp):
-	print "The Heat of Solution is " + str((((4.2 * gramsWater * (finalTemp - initTemp))/(gramsCompound / molarMass))/1000) * -1) + " kJ/mol."
+	print "The Heat of Solution is "+str((((4.2*gramsWater*(finalTemp-initTemp))/(gramsCompound/molarMass))/1000)*-1)+" kJ/mol."
 	return
-calcEnthalpy(molarMass=40.0,gramsCompound=3.0,gramsWater=50.0,initTemp=0.0,finalTemp=12.5)
+calcEnthalpy(molarMass=80.052,gramsCompound=2.0,gramsWater=20.0,initTemp=0.0,finalTemp=-3.2)
