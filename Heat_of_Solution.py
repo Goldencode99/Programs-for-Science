9a2ced53d2cf5e71e2a96f5c491d10f347f1f967
Goldencode99 <auves99@icloud.com>
2014-04-28 16:42:01 -0700

    Improved usability of Heat of Solution program.
    Added user input from Terminal, as opposed to direct input into the
    program.

--- Heat_of_Solution.py
+++ Heat_of_Solution.py
@@ -1,4 +1,10 @@
 def calcEnthalpy(molarMass,gramsCompound,gramsWater,initTemp,finalTemp):
 	print "The Heat of Solution is "+str((((4.2*gramsWater*(finalTemp-initTemp))/(gramsCompound/molarMass))/1000)*-1)+" kJ/mol."
 	return
-calcEnthalpy(molarMass=80.052,gramsCompound=2.0,gramsWater=20.0,initTemp=0.0,finalTemp=-3.2)
+print "This program can be used to calculate the \"Heat of Solution\" for a given compound.\nNOTE: Please enter all values to at least the first decimal."
+molarMass = input("Enter molar mass of compound: ")
+gramsCompound = input("Enter grams of compound used: ")
+gramsWater = input("Enter grams of water used: ")
+initTemp = input("Enter initial temperature of solution: ")
+finalTemp = input("Enter final temperature of solution: ")
+calcEnthalpy(molarMass=molarMass,gramsCompound=gramsCompound,gramsWater=gramsWater,initTemp=initTemp,finalTemp=finalTemp)
